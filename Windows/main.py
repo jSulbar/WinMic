acc5ddfb5a9bd812543b5d8d3fba0860df3a8d21
jSulbar <diosmiolacreatura@gmail.com>
2021-11-07 21:09:32 -0400

    Update app object to MicApp

--- Windows/main.py
+++ Windows/main.py
@@ -1,19 +1,19 @@
-import wx
 from gui.app_frame import MicFrame
 from client.gui_backend import BackendConnector
+from client.mic_app import MicApp
 
 
 if __name__ == '__main__':
 
     # Start wxPython app
-    app = wx.App()
+    app = MicApp(appname='WinMic')
     
     # Init window and add controls
     window = MicFrame()
     window.init_gui()
     window.add_controls()
 
-    backend = BackendConnector(window)
+    backend = BackendConnector(window, app)
     backend.setup()
 
     window.Show()
