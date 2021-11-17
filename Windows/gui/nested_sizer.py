9c665a4816be2d7a5ce479451ba1fa4c3e77b3fc
jSulbar <diosmiolacreatura@gmail.com>
2021-11-17 00:02:23 -0400

    add docstrings

--- Windows/gui/nested_sizer.py
+++ Windows/gui/nested_sizer.py
@@ -2,6 +2,9 @@ import wx
 
 # Custom sizer to help manage and search for elements
 class ChildSizer(wx.BoxSizer):
+    """Custom BoxSizer to be contained inside a ParentSizer instance. Controls should be added with
+    csizer.add_control() instead of wxPython's default sizer.Add(). Each control must be added with
+    an unique name to identify it. These can then be retrieved anytime using csizer.get_control."""
     def __init__(self, sizer = wx.HORIZONTAL) -> None:
         super().__init__(sizer)
         # Dict with every control that has been added
@@ -23,6 +26,10 @@ class ChildSizer(wx.BoxSizer):
 # A parent sizer class to hold other sizers,
 # and to make it less repetitive to add controls
 class ParentSizer(wx.BoxSizer):
+    """Custom BoxSizer that contains ChildSizer instances inside of itself. Controls should be
+    added with psizer.new_control() or psizer.new_controls_row(). It implicitly creates a new ChildSizer
+    with each control or controls added to avoid verbose object creation and handling, and maintains
+    references to each control added which can then be retrieved with control_by_name()."""
     def __init__(self, sizer = wx.VERTICAL) -> None:
         super().__init__(sizer)
         # List of child sizers to keep track of
@@ -41,6 +48,8 @@ class ParentSizer(wx.BoxSizer):
     # Search inside child sizers for a control
     # with the given name
     def control_by_name(self, name):
+        """Returns a control added to this sizer, by searching for a control with
+        the name provided."""
         for childsizer in self._csizer_list:
             for key in childsizer.control_list:
                 if key == name:
@@ -53,6 +62,18 @@ class ParentSizer(wx.BoxSizer):
 
     # Add a new control
     def new_control(self, control, name, *, sizer_options, control_options):
+        """Adds a new control to this sizer, creating a new ChildSizer containing it. 
+        Takes arguments as follows:\n
+
+        control: The new wx.Control added.\n
+
+        name: An identifying string used to search the control with control_by_name().\n
+
+        sizer_options: A tuple containing arguments that determine how the new ChildSizer created
+        will fit into this ParentSizer. See wxPython.BoxSizer docs for possible values.\n   
+
+        control_options: A tuple with arguments defining how the new controls will fit
+        into the new ChildSizer."""
         if isinstance(control, wx.Control):
             # Create a new sizer for it
             new_sizer = ChildSizer(wx.HORIZONTAL)
@@ -65,6 +86,17 @@ class ParentSizer(wx.BoxSizer):
     # Add multiple controls and put them inside a single sizer
     # Takes a dict formatted as { "controlname":control }
     def new_controls_row(self, controls, *, sizer_options, control_options):
+        """Adds many controls to this sizer, creating a single ChildSizer containg each control.
+        Takes arguments as follows:\n
+
+        controls: Takes a dictionary, with each key being the control's identifying name and
+        their values containing the control object.\n
+
+        sizer_options: A tuple containing arguments that determine how the new ChildSizer will fit
+        into this ParentSizer. See wxPython.BoxSizer docs for possible values.\n
+
+        control_options: A tuple with arguments defining how the new controls will fit
+        into the new ChildSizer."""
         if type(controls) is dict:
             new_sizer = ChildSizer(wx.HORIZONTAL)
 
