03b7f45bb7860cefcd14c647bfd3757f4f70ab42
jSulbar <diosmiolacreatura@gmail.com>
2021-11-23 10:55:21 -0400

    remove wx.icon creation

--- Windows/gui/tray_icon.py
+++ Windows/gui/tray_icon.py
@@ -1,16 +1,16 @@
 import wx
 import wx.adv
+from constants import APP_NAME
 
 
 # Class for the system tray icon of this app
 class TrayIcon(wx.adv.TaskBarIcon):
-    def __init__(self, frame):
+    def __init__(self, frame, icon):
         wx.adv.TaskBarIcon.__init__(self)
         self.frame = frame
-        self.icon = wx.Icon()
 
         # Show micWindow on left click
-        self.SetIcon(self.icon, 'WinMic')
+        self.SetIcon(self.icon, APP_NAME)
         self.Bind(wx.adv.EVT_TASKBAR_LEFT_DOWN, self.show_window)
 
         # Display options on right
