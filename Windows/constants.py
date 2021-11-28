fe202a2b5a985a1adf00bbcc66f81ea3080003bd
jSulbar <diosmiolacreatura@gmail.com>
2021-11-28 10:15:42 -0400

    update constants.py

--- Windows/constants.py
+++ Windows/constants.py
@@ -7,6 +7,13 @@ import wx
 APP_NAME = 'WinMic'
 DEFAULT_PORT = 12358
 
+HOST_APIS = {
+    'ASIO': pyaudio.paASIO,
+    'DirectSound': pyaudio.paDirectSound,
+    'MME': pyaudio.paMME,
+    'PADevel': pyaudio.paInDevelopment
+}
+
 LOCALE_DIR_NAME = 'locale'
 AVAILABLE_LANGS = {
     'Spanish': wx.LANGUAGE_SPANISH,
@@ -15,9 +22,11 @@ AVAILABLE_LANGS = {
 CATALOG_NAME = 'i18n'
 
 CFGKEY_LANGUAGE = 'language'
+CFGKEY_HOSTAPI = 'host_api'
 CFGKEY_TRAY = 'minimize_to_tray'
 DEFAULT_APP_CONFIG = {
     CFGKEY_LANGUAGE: str(wx.LANGUAGE_DEFAULT),
-    CFGKEY_TRAY: 'True'
+    CFGKEY_TRAY: 'True',
+    CFGKEY_HOSTAPI: str(HOST_APIS['ASIO'])
 }
 CFG_FILENAME = 'cfg.ini'
