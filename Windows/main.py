a348a3c9044d17a0efc2f34dcd9161f1bdccd7fe
jSulbar <diosmiolacreatura@gmail.com>
2021-11-28 10:41:14 -0400

    update main

--- Windows/main.py
+++ Windows/main.py
@@ -4,70 +4,76 @@ from gui.layout_definitions import MAIN_WINDOW_LAYOUT
 from gui.nested_sizer import ParentSizer
 from gui.tray_icon import TrayIcon
 from gui.control_factory import ControlFactory
+from gui.menus import OptionsMenu
 
 from client.config_file import WinMicConfig
-from client.gui_backend import MainWindowBackend
+from client.gui_backend import MainWindowBackend, OptionsController
 from client.udp_socket import DatagramSocket
 from client.mic_player import MicPlayer
 from client.winmic_app import WinMicApp
 
 from constants import APP_NAME, DEFAULT_APP_CONFIG, DEFAULT_PORT, CFG_FILENAME
 
-
-def parse_ctrls(ctrl_defs):
-        ctrls_dict = {}
-        for ctrl in ctrl_defs:
-            if 'control' in ctrl:
-                ctrls_dict[ctrl['name']] = ctrl['control']
-            elif 'controls' in ctrl:
-                for key in ctrl['controls']:
-                    ctrls_dict[key] = ctrl['controls'][key]
-        return ctrls_dict
-
+_ = wx.GetTranslation
 
 if __name__ == '__main__':
 
+    # Init wxPython app
     app = WinMicApp()
+    appicon = wx.Icon('winmic.ico')
 
     # Make unresizable window, set title
     window = wx.Frame(None, style=wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
     window.SetTitle(APP_NAME)
+    window.SetIcon(appicon)
     window.Center()
 
     # Read language from config and set it
     cfg = WinMicConfig(APP_NAME, CFG_FILENAME, DEFAULT_APP_CONFIG)
-    lang = int(cfg.Read('language'))
+    lang = cfg.get_language()
     app.set_lang(lang)
 
+    # Create boxsizer and control container for main window
     micframe_sizer = ParentSizer()
     panel = wx.Panel(window)
 
-    # Create controls for main window and add to sizer
+    # Create controls for main window and add them to its sizer
     factory = ControlFactory(panel)
     controls = factory.get_controls(MAIN_WINDOW_LAYOUT)
     micframe_sizer.controls_from_factory(controls)
-    parsedctrls = parse_ctrls(controls)
 
-    #TODO: fix duct tape
-    parsedctrls['ip_label'].LabelText += DatagramSocket.get_local_ip()
+    tray_enabled = cfg.get_tray_cfg()
+    host_api = cfg.get_host_api()
 
+    # Create menu bar, add settings menu to it
+    menu_bar = wx.MenuBar()
+    options_menu = OptionsMenu(tray_enabled, lang, host_api)
+    menu_bar.Append(options_menu, _('Options'))
+
+    window.SetMenuBar(menu_bar)
+
+    # Lock the sizer onto the main window
+    micframe_sizer.SetSizeHints(panel)
+    window.SetSizerAndFit(micframe_sizer)
+
+    # Create tray icon for the app
+    tray = TrayIcon(window, appicon)
+
+    # Init socket and micplayer
     sock = DatagramSocket(DEFAULT_PORT)
     sock.bind(('', sock.port))
     micplayer = MicPlayer()
 
-    # Read from config if the window should be sent to tray or closed.
-    # TODO: fix also this duct tape
-    tray_cfg = cfg.Read('minimize_to_tray') == 'True'
-    window_backend = MainWindowBackend(micplayer, sock, tray_cfg)
-    window_backend.inject_controls(parsedctrls)
+    # Start main window's "business logic" class
+    window_backend = MainWindowBackend(micplayer, sock, cfg)
+    window_backend.inject_controls(controls)
+    window_backend.fill_control_data()
 
+    # Bind main window's GUI to underlying logic
     window_backend.bind_controls()
-    window_backend.bind_mainwindow_close(window)
-
-    tray = TrayIcon(window)
-
-    micframe_sizer.SetSizeHints(panel)
-    window.SetSizerAndFit(micframe_sizer)
+    window_backend.bind_mainwindow_close(window, tray)
+    options_backend = OptionsController(options_menu, cfg)
+    options_backend.bind_controls()
 
     window.Show()
     app.MainLoop()
